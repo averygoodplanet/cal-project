85728af50833020e23a433d983a83a14b6da4e32
averygoodplanet <averygoodplanet@gmail.com>
2013-12-19 20:47:33 -0600

    basic EMPTY testing structure

---
+++ program_file.rb
@@ -0,0 +1,6 @@
+# Program file
+
+# # Example program file for integration testing:
+# puts = "Whats your name?"
+# name = gets
+# puts name
---
+++ test/test_integration.rb
@@ -0,0 +1,35 @@
+# Run this integration test by typing "rake".
+# Note that per Rakefile, these integration test
+# filenames must be in path/naming convention
+# of "test/test_*.rb"
+
+# helper.rb includes helper functions
+require_relative 'helper'
+
+class TestIntegration < MiniTest::Unit::TestCase
+# *** NOTE: the name of program file tested
+# is below in IO.popen('ruby program_file.rb' ...)...
+
+
+## ****Example Unit Test on ./program_file.rb
+#   def test_a_name_with_no_vowels
+#     shell_output = ""
+#     # the from IO.popen... to first "end" is the test
+#     # simulating the person typing entering "brt"
+#     # on name = gets.chomp
+#     IO.popen('ruby program_file.rb', 'r+') do |pipe|
+#       pipe.puts("brt")
+#       pipe.close_write
+#       shell_output = pipe.read
+#     end
+#     expected_output = <<EOS
+#    b
+#     r
+#     t
+# EOS
+#     #I'm not sure if this should be
+#     #  .... expected_output, shell_output
+#     # or vice versa order
+#     assert_equal expected_output, shell_output
+#   end
+end
