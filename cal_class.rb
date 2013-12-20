36e02f15b73e1a661e79e28cb493c9d2e24cc265
averygoodplanet <averygoodplanet@gmail.com>
2013-12-19 23:20:14 -0600

    corrected formula to software style Zeller formula

--- cal_class.rb
+++ cal_class.rb
@@ -8,32 +8,19 @@ class Cal
     # uses Zeller Congruence for Gregorian (modern) Calendar formula to return
     # the day-of-week a month-year starts on
     # see http://en.wikipedia.org/wiki/Zeller's_congruence
+    # see formulas under "Implementation in software"
 
     #assign variables other than h, which is the result of the Zeller equation
     if month <= 2
       zeller_month = month + 12
-      puts "in month <= 2, zeller_month= " + zeller_month.to_s
+      year = year - 1
     else
       zeller_month = month
     end
 
     first_day_of_month = 1
     day_of_month = first_day_of_month
-    puts "day_of_month= " + day_of_month.to_s
-    year_of_century = year % 100
-    puts "year_of_century = " + year_of_century.to_s
-    century = year / 100
-    puts "century= " + century.to_s
 
-
-    zeller_day_of_week_index = (day_of_month + ((13*(zeller_month + 1))/5).floor + year_of_century + (year_of_century/4).floor + (century/4).floor - (2 * century) ) % 7
-    zeller_day_of_week_index - 2
-
-    # try_regular_month = (day_of_month + ((13*(month + 1))/5).floor + year_of_century + (year_of_century/4).floor + (century/4).floor - (2 * century) ) % 7
-    # zmonth_plus_one = zeller_month + 1
-    # try_parens = (day_of_month + ((13*zmonth_plus_one) / 5 ) + year_of_century + (year_of_century/4).floor + ((100 * century)/4).floor - (2 * (century *100) )) % 7
-    # try_parens - 2
-    # zeller_hash = { }
-    #return day-of-week the month starts on
+    zeller_day_of_week_index = (day_of_month + (((zeller_month + 1) * 26)/ 10) + year + (year/4).floor + (6 * ((year/100).floor)) + (year/400).floor) % 7
   end
 end
