b7cd792369d7efe3839a4d36ade79abf932a92ac
averygoodplanet <averygoodplanet@gmail.com>
2013-12-19 23:49:05 -0600

    shell Unix and display my zeller result

--- cal.rb
+++ cal.rb
@@ -1,6 +1,13 @@
-# Program file
+# Program file (setup for integration testing when typing rake)
+require_relative 'cal_class.rb'
 
-# # Example program file on which doing integration testing:
-# puts = "Whats your name?"
-# name = gets
-# puts name
+#accepting arguments from command line, e.g. "ruby cal.rb month year"
+month = ARGV[0]
+year = ARGV[1]
+
+#using tick marks (tilde key) to shell commands
+puts "first argument: #{month}"
+puts "second argument: #{year}"
+puts "now shelling to Unix cal, Unix's result"
+puts `cal #{month} #{year}`
+puts  "***My zeller result:  " + Cal.zeller(month.to_i, year.to_i, :full).to_s
