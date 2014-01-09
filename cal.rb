59da8dc3cf7b1944bd8dbad30b930138aa7c40e2
averygoodplanet <averygoodplanet@gmail.com>
2014-01-09 17:26:33 -0600

    refactor date scope with last_argument variable

--- cal.rb
+++ cal.rb
@@ -4,23 +4,20 @@ require_relative 'cal_class.rb'
 #accepting arguments from command line, e.g. "ruby cal.rb month year"
 first_argument = ARGV[0].to_i
 second_argument = ARGV[1].to_i
+last_argument = ARGV[ARGV.length - 1].to_i
 
-if ARGV.length == 2
-  if second_argument > 3000 || second_argument < 1800
-    puts "Date is out-of-scope (years 1800-3000)."
-  else
-    Cal.display_month(first_argument, second_argument)
-  end
+if last_argument > 3000 || last_argument < 1800
+  puts "Date is out-of-scope (years 1800-3000)."
 else
-  if first_argument > 3000 || first_argument < 1800
-    puts "Date is out-of-scope (years 1800-3000)."
+  if ARGV.length == 2
+    Cal.display_month(first_argument, second_argument)
   else
     Cal.display_year(first_argument)
   end
 end
 
-# Example for Future Reference
-# #using tick marks (tilde key) to shell commands
+# Example for Future Reference:
+# using tick marks (tilde key) to shell commands
 # puts "first argument: #{month}"
 # puts "second argument: #{year}"
 # puts "now shelling to Unix cal, Unix's result"
