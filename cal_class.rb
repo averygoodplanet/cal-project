6f88c912e8ac8a6e6041c13c6ee3c34e98384200
averygoodplanet <averygoodplanet@gmail.com>
2014-01-10 15:30:06 -0600

    refactor: 80 char column width

--- cal_class.rb
+++ cal_class.rb
@@ -3,9 +3,11 @@ class Cal
   def self.zeller(month, year)
     # uses Zeller Congruence for Gregorian (modern) Calendar formula to return
     # the day-of-week a month-year starts on
-    # see http://en.wikipedia.org/wiki/Zeller's_congruence, formulas under "Implementation in software"
+    # see http://en.wikipedia.org/wiki/Zeller's_congruence,
+    #formulas under "Implementation in software"
 
-    # in Zeller's Congruence formula months are numbered: (3 = March, 4 = April, 5 = May, ..., 14 = February)
+    # in Zeller's Congruence formula months are numbered:
+    #(3 = March, 4 = April, 5 = May, ..., 14 = February)
     if month <= 2
       months_in_a_year = 12
       zeller_month = month + months_in_a_year
@@ -14,7 +16,9 @@ class Cal
       zeller_month = month
     end
 
-    zeller_day_of_week_index = (1 + (((zeller_month + 1) * 26)/ 10) + year + (year/4).floor + (6 * ((year/100).floor)) + (year/400).floor) % 7
+    zeller_day_of_week_index = (1 + (((zeller_month + 1) * 26)/ 10) + year +
+                                                     (year/4).floor + (6 * ((year/100).floor)) +
+                                                     (year/400).floor) % 7
   end
 
   def self.get_month_strings_array(month_number, year_number)
@@ -22,7 +26,9 @@ class Cal
     month_name = Cal.get_month_name(month_number)
 
     leading_whitespace = ""
-    leading_spaces_by_month = { 1 => 4, 2 => 3, 3 => 5, 4 => 5, 5 => 6,  6 => 5, 7 => 5, 8 => 4, 9 => 3, 10 => 4, 11 => 3, 12 => 3}
+    leading_spaces_by_month = { 1 => 4, 2 => 3, 3 => 5, 4 => 5, 5 => 6,  6 => 5,
+                                                     7 => 5, 8 => 4, 9 => 3, 10 => 4, 11 => 3,
+                                                     12 => 3}
     number_of_leading_spaces = leading_spaces_by_month[month_number]
     for i in 1..number_of_leading_spaces
       leading_whitespace += " "
@@ -35,12 +41,14 @@ class Cal
     month_rows[0] = month_header_string
     month_rows[1] = weekday_names_row
 
-    days_in_this_month = Cal.number_of_days_in_month(month_number, year_number)
+    days_in_this_month = Cal.number_of_days_in_month(month_number,
+                                         year_number)
     first_day_of_month = Cal.zeller(month_number, year_number)
     first_day_of_month = 7 if first_day_of_month == 0
 
     if first_day_of_month == 1
-      month_rows.push *[" 1  2  3  4  5  6  7", " 8  9 10 11 12 13 14", "15 16 17 18 19 20 21", "22 23 24 25 26 27 28"]
+      month_rows.push *[" 1  2  3  4  5  6  7", " 8  9 10 11 12 13 14",
+                                        "15 16 17 18 19 20 21", "22 23 24 25 26 27 28"]
 
       if days_in_this_month == 31
         month_rows[6] = "29 30 31"
@@ -101,19 +109,24 @@ class Cal
 end
 
   def self.display_month(month_number, year_number)
-    leading_spaces_by_month = { 1 => 4, 2 => 3, 3 => 5, 4 => 5, 5 => 6,  6 => 5, 7 => 5, 8 => 4, 9 => 3, 10 => 4, 11 => 3, 12 => 3}
+    leading_spaces_by_month = { 1 => 4, 2 => 3, 3 => 5, 4 => 5, 5 => 6,  6 => 5,
+                                                     7 => 5, 8 => 4, 9 => 3, 10 => 4, 11 => 3,
+                                                     12 => 3}
     number_of_leading_spaces = leading_spaces_by_month[month_number]
 
-    puts " " * number_of_leading_spaces + Cal.get_month_name(month_number) + " " + year_number.to_s
+    puts " " * number_of_leading_spaces + Cal.get_month_name(month_number) +
+             " " + year_number.to_s
     puts "Su Mo Tu We Th Fr Sa"
 
-    days_in_this_month = Cal.number_of_days_in_month(month_number, year_number)
+    days_in_this_month = Cal.number_of_days_in_month(month_number,
+                                         year_number)
     first_day_of_month = Cal.zeller(month_number, year_number)
     first_day_of_month = 7 if first_day_of_month == 0
 
     week_rows = []
     if first_day_of_month == 1
-      week_rows.push *[" 1  2  3  4  5  6  7", " 8  9 10 11 12 13 14", "15 16 17 18 19 20 21", "22 23 24 25 26 27 28"]
+      week_rows.push *[" 1  2  3  4  5  6  7", " 8  9 10 11 12 13 14",
+                                    "15 16 17 18 19 20 21", "22 23 24 25 26 27 28"]
 
       case days_in_this_month
       when 31
@@ -177,69 +190,77 @@ end
   def self.display_year(year)
     puts "                             #{year}"
 
-    # handles edge case where we don't place a spacer row after quarter year if contains a month with six week rows
+    # handles edge case where we don't place a spacer row after quarter year if
+    # contains a month with six week rows
     six_weeks_row = false
 
     for quarter_of_year in [0, 3, 6, 9]
-      month_index = quarter_of_year
+      q = quarter_of_year
       #for each trimester
       puts "\n" if !six_weeks_row
 
       six_weeks_row = false
-      months_string_arrays = []
-      left_month, center_month, right_month = month_index, month_index + 1, month_index + 2
+      month_strings = []
+      left_month, center_month, right_month = q, q + 1, q + 2
 
         for i in left_month..right_month
-          months_string_arrays[i] = self.get_month_strings_array(i+1, year)
+          month_strings[i] = self.get_month_strings_array(i+1, year)
         end
 
       for row in 0..5
-        self.concatenate_and_puts(row, months_string_arrays, left_month, center_month, right_month)
+        self.concatenate_and_puts(row, month_strings, left_month,
+                                                      center_month, right_month)
       end
 
       # Handle edge cases where a month has only four (4) or has six (6) week rows
       for row in 6..7
-        if !months_string_arrays[left_month][row].nil? or !months_string_arrays[center_month][row].nil? or !months_string_arrays[right_month][row].nil?
+        if !month_strings[left_month][row].nil? or
+          !month_strings[center_month][row].nil? or
+          !month_strings[right_month][row].nil?
           six_weeks_row = true if row == 7
-          for month_index in left_month..right_month
-            months_string_arrays[month_index][row] = "".ljust(20, " ") if months_string_arrays[month_index][row].nil?
+          for q in left_month..right_month
+            month_strings[q][row] = "".ljust(20, " ") if month_strings[q][row].nil?
           end
-          self.concatenate_and_puts(row, months_string_arrays, left_month, center_month, right_month)
+          self.concatenate_and_puts(row, month_strings, left_month, center_month,
+            right_month)
         end
       end
     end
     puts "\n"
   end
 
-  def self.concatenate_and_puts(row, months_string_arrays, left_month, center_month, right_month)
-    concatenated_strings_array = []
-    concatenated_strings_array[row] = months_string_arrays[left_month][row].ljust(20, " ").chomp + "  " +
-                                                                months_string_arrays[center_month][row].ljust(20, " ").chomp + "  " +
-                                                                months_string_arrays[right_month][row].ljust(20, " ").sub(/\s+\Z/, "")
-    puts concatenated_strings_array[row]
+  def self.concatenate_and_puts(row, month_strings, left_month, center_month,
+    right_month)
+    concat_strings = []
+    concat_strings[row] = month_strings[left_month][row].ljust(20, " ").chomp +
+                          "  " +month_strings[center_month][row].ljust(20, " ").chomp +
+                          "  " +month_strings[right_month][row].ljust(20, " ").sub(/\s+\Z/, "")
+    puts concat_strings[row]
   end
 
   def self.get_month_name(month_number)
-    month_name_hash = { 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August",
-                                         9 => "September", 10 => "October", 11 => "November", 12 => "December"}
+    month_name_hash = { 1 => "January", 2 => "February", 3 => "March",
+          4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August",
+          9 => "September", 10 => "October", 11 => "November", 12 => "December"}
     month_name_hash[month_number]
   end
 
   def self.leap_year?(year)
-    multiple_of_4 = ((year % 4) == 0)
+    mult_4 = ((year % 4) == 0)
     century = ((year % 100) == 0)
-    multiple_of_400 = ((year % 400) == 0)
+    mult_400 = ((year % 400) == 0)
 
-    (multiple_of_4 and !century) or (multiple_of_4 and century and multiple_of_400) ? true : false
+    (mult_4 and !century) or (mult_4 and century and mult_400) ? true : false
   end
 
   def self.number_of_days_in_month(month, year)
-    month_days_hash = { 1 => 31, 2 => "February", 3 => 31, 4 => 30, 5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
+    month_days_hash = { 1 => 31, 2 => "February", 3 => 31, 4 => 30, 5 => 31,
+                    6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31, 11 => 30, 12 => 31}
 
     if month_days_hash[month] != "February"
-      this_month_number_days = month_days_hash[month]
+      num_days = month_days_hash[month]
     else
-      Cal.leap_year?(year) ? this_month_number_days = 29 : this_month_number_days = 28
+      Cal.leap_year?(year) ? num_days = 29 : num_days = 28
     end
   end
 end
