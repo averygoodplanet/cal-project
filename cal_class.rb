57c1b91516e5f9bbdfed45d28e1c840847047396
averygoodplanet <averygoodplanet@gmail.com>
2014-01-13 11:46:43 -0600

    refactor: get_month_string... mimic display_month

--- cal_class.rb
+++ cal_class.rb
@@ -22,91 +22,22 @@ class Cal
   end
 
   def self.get_month_strings_array(month_number, year_number)
-
-    month_name = Cal.get_month_name(month_number)
-
-    leading_whitespace = ""
-    leading_spaces_by_month = { 1 => 4, 2 => 3, 3 => 5, 4 => 5, 5 => 6,  6 => 5,
-                                                     7 => 5, 8 => 4, 9 => 3, 10 => 4, 11 => 3,
-                                                     12 => 3}
-    number_of_leading_spaces = leading_spaces_by_month[month_number]
-    for i in 1..number_of_leading_spaces
-      leading_whitespace += " "
-    end
-
-    month_header_string = month_name.center(20)
-    weekday_names_row = "Su Mo Tu We Th Fr Sa"
-
-    month_rows = []
-    month_rows[0] = month_header_string
-    month_rows[1] = weekday_names_row
-
     days_in_this_month = Cal.number_of_days_in_month(month_number,
                                          year_number)
     first_day_of_month = Cal.zeller(month_number, year_number)
     first_day_of_month = 7 if first_day_of_month == 0
+    last_number_in_first_row = 7 - (first_day_of_month - 1)
 
-    if first_day_of_month == 1
-      month_rows.push *[" 1  2  3  4  5  6  7", " 8  9 10 11 12 13 14",
-                                        "15 16 17 18 19 20 21", "22 23 24 25 26 27 28"]
+    week_rows = []
+    week_rows[0] = Cal.make_month_first_row(first_day_of_month, last_number_in_first_row)
+    week_rows, last_number_middle_rows = Cal.add_month_middle_rows(week_rows,
+      last_number_in_first_row, days_in_this_month)
+    week_rows = Cal.add_month_partial_last_row(week_rows,last_number_middle_rows,
+     days_in_this_month) if last_number_middle_rows < days_in_this_month
 
-      if days_in_this_month == 31
-        month_rows[6] = "29 30 31"
-      elsif days_in_this_month == 30
-        month_rows[6] = "29 30"
-      elsif days_in_this_month == 29
-        month_rows[6] = "29"
-      else
-      end
-    else
-      month_rows[2] = ""
-
-      # make first row's leading spaces (3 spaces per grouping)
-      for i in 1..(first_day_of_month - 1)
-        month_rows[2] += "   "
-      end
-
-      last_number_in_first_row = 7 - (first_day_of_month -1)
-
-      for i in 1..last_number_in_first_row
-        month_rows[2] += " #{i} "
-      end
-
-      month_rows[2] = month_rows[2].chop
-      current_week_row = 3
-
-      # make middle full rows in month
-      first_in_row = last_number_in_first_row + 1
-      last_number_middle_rows = 0
-      while (first_in_row + 6) <= days_in_this_month do
-        week_string = ""
-        for i in first_in_row..(first_in_row + 6)
-            if i < 10
-              number_string = " " + i.to_s
-           else
-              number_string = i.to_s
-           end
-          week_string += number_string + " "
-          last_number_middle_rows = number_string.to_i
-        end
-        week_string = week_string.chop
-        month_rows[current_week_row] = week_string
-        current_week_row += 1
-        first_in_row += 7
-      end
-
-      #make less-than-full ending row
-      if last_number_middle_rows < days_in_this_month
-        first_in_row = last_number_middle_rows + 1
-        last_week_array = []
-        for i in first_in_row..days_in_this_month
-          last_week_array.push(i)
-        end
-        month_rows.push(last_week_array.join(" "))
-      end
+    week_rows.unshift( Cal.get_month_name(month_number).center(20),
+              "Su Mo Tu We Th Fr Sa")
   end
-  month_rows
-end
 
   def self.display_month(month_number, year_number)
     days_in_this_month = Cal.number_of_days_in_month(month_number,
